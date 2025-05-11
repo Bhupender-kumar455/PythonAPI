0e5408ba00b26b069a7c09f0f2f311b513ab8c4d
Bhupender-kumar455 <kbhupender455@gmail.com>
2025-05-11 19:51:57 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, send_file
+from rembg import remove
+from PIL import Image
+import io
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/remove-bg', methods=['POST'])
+def remove_background():
+    if 'image' not in request.files:
+        return {'error': 'No image provided'}, 400
+
+    file = request.files['image']
+    input_image = Image.open(file.stream)
+    output_image = remove(input_image)
+
+    output_buffer = io.BytesIO()
+    output_image.save(output_buffer, format='PNG')
+    output_buffer.seek(0)
+
+    return send_file(
+        output_buffer,
+        mimetype='image/png',
+        as_attachment=True,
+        download_name='output.png'
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
